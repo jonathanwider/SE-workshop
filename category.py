dfc060f40bb6a39c4ac106aec0a4830dafbf6fab
Martin Lorenz <25765799+martinoz1024@users.noreply.github.com>
2023-10-04 11:51:42 +0200

    fix: mutable default value behaviour
    By design default values in Python are evaluated when, and only when the „def“ statement they belong to is executed.
    
    https://web.archive.org/web/20200221224620id_/http://effbot.org/zone/default-values.htm

--- category.py
+++ category.py
@@ -1,11 +1,13 @@
 from question import Question
 class Category(object):
-    def __init__(self, title: str, questions: [Question] = []):
+    def __init__(self, title: str, questions: [Question] = None):
         """
 
         :param title: The category's title (e.g. sports)
         :param questions: The questions of the category, optional for init, can be added later with :func:`add_question <category.Category.add_question>`
         """
+        if questions == None:
+            questions = []
         self.title = title
         self.questions = questions
         self._currentQuestion = 0
